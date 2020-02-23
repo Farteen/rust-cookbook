a37d2f26b12adc110352096c012fd90e9a38328f
farteen <xujavacn@gmail.com>
2020-02-23 10:53:47 +0800

    builder pattern

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,132 @@
 fn main() {
-    // let name_length = 
+    let normal_burger = BurgerBuilder::new().build();
+    
+    let cheese_burger = BurgerBuilder::new()
+    .cheese(true)
+    .salad(false)
+    .build();
+
+    let veggie_bigmac = BurgerBuilder::new()
+    .vegetarian(true)
+    .patty_count(2)
+    .build();
+
+    if let Ok(normal_burger) = normal_burger {
+        normal_burger.print();
+    }
+
+    if let Ok(cheese_burger) = cheese_burger {
+        cheese_burger.print();
+    }
+
+    if let Ok(veggie_bigmac) = veggie_bigmac {
+        veggie_bigmac.print();
+    }
+
+    let invalid_burger = BurgerBuilder::new()
+    .vegetarian(true)
+    .bacon(true)
+    .build();
+
+    if let Err(error) = invalid_burger {
+        println!("Failed to print burger: {}", error);
+    }
+
+    let cheese_burger_builder = BurgerBuilder::new().cheese(true);
+    for i in 1..10 {
+        let cheese_burger = cheese_burger_builder.build();
+        if let Ok(cheese_burger) = cheese_burger {
+            println!("cheese burger number {} is ready", i);
+            cheese_burger.print();
+        }
+    }
 }
 
-struct NameLength {
-    name: String,
-    length: usize,
+struct Burger {
+    patty_count: i32,
+    vegetarian: bool,
+    cheese: bool,
+    bacon: bool,
+    salad: bool,
 }
 
-impl NameLength {
-    fn new(name: &str) -> Self {
-        NameLength{
-            name: name.to_owned(),
-            length: name.len(),
+impl Burger {
+    fn print(&self) {
+        let pretty_patties = if self.patty_count == 1 {
+            "patty"
+        } else {
+            "patties"
+        };
+
+        let pretty_bool = |val| if val { "" } else { "no " };
+        let pretty_vegetarian = if self.vegetarian { "vegetarian " } else { "" };
+        println!("This is a {}burger with {} {}, {} cheese, {} bacon and {} salad", 
+            pretty_vegetarian,
+            self.patty_count,
+            pretty_patties,
+            pretty_bool(self.cheese),
+            pretty_bool(self.bacon),
+            pretty_bool(self.salad)
+        );
+    }
+}
+
+struct BurgerBuilder {
+    patty_count: i32,
+    vegetarian: bool,
+    cheese: bool,
+    bacon: bool,
+    salad: bool,
+}
+
+impl BurgerBuilder {
+    fn new() -> Self {
+        BurgerBuilder {
+            patty_count: 1,
+            vegetarian: false,
+            cheese: false,
+            bacon: false,
+            salad: true,
         }
     }
 
-    fn print(&self) {
-        println!("The name '{}' is '{}' characters long", self.name, self.length);
+    fn patty_count(mut self, val: i32) -> Self {
+        self.patty_count = val;
+        self
     }
-}
 
-use std::borrow::Cow;
-struct NameLengthLT<'a> {
-    name: Cow<'a, str>,
-    length: usize,
-}
+    fn vegetarian(mut self, val: bool) -> Self {
+        self.vegetarian = val;
+        self
+    }
 
-impl<'a> NameLengthLT<'a> {
-    fn new<S>(name: S) -> Self
-    where 
-        S: Into<Cow<'a, str>>, {
-            let name: Cow<'a, str> = name.into();
-            /**
-             * watch out the diffs below
-            NameLengthLT {
-                length: name.len(),
-                name,
-            }
-             */
-            NameLengthLT {
-                length: name.len(),
-                name,
-            }
+    fn cheese(mut self, val: bool) -> Self {
+        self.cheese = val;
+        self
+    }
+
+    fn salad(mut self, val: bool) -> Self {
+        self.salad = val;
+        self
+    }
+
+    fn bacon(mut self, val: bool) -> Self {
+        self.bacon = val;
+        self
+    }
+
+    fn build(&self) -> Result<Burger, String> {
+        let burger = Burger {
+            patty_count: self.patty_count,
+            vegetarian: self.vegetarian,
+            cheese: self.cheese,
+            bacon: self.bacon,
+            salad: self.salad,
+        };
+        if burger.vegetarian && burger.bacon {
+            Err("Sorry, but we dont server vegetarian bacon yet".to_string())
+        } else {
+            Ok(burger)
         }
-
-    fn print(&self) {
-        println!("The name '{}‘ is '{}' characters long", self.name, self.length);
     }
 }
