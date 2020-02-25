58c9c332c28a9bfc03c2de425eb2cf3f0db56827
farteen <xujavacn@gmail.com>
2020-02-25 22:55:33 +0800

    using string

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,124 @@
-fn main() {
-    let fruits = vec!["apple", "tomato", "pear"];
+fn main () {
+    let mut s = String::new();
+    s.push('H');
+    s.push('I');
+    println!("s: {}", s);
 
-    println!("Fruits: {:?}", fruits);
+    let s = "Hello".to_string();
+    println!("s: {}", s);
 
-    let mut fruits = Vec::new();
-    fruits.push("apple");
-    fruits.push("tomato");
-    fruits.push("pear");
-    println!("fruits: {:?}", fruits);
+    let s = String::from("Hello");
+    println!("s: {}", s);
 
-    let last = fruits.pop();
-    if let Some(last) = last {
-        println!("Removed {} from {:?}", last, fruits);
+    let s = "汉语";
+    println!("s: {}", s);
+
+    let mut s = "Hello ".to_string();
+    s.push_str("World");
+
+    for ch in "Tubular".chars() {
+        println!("{}", ch);
     }
 
-    fruits.insert(1, "grape");
-    println!("fruits after insertion: {:?}", fruits);
-
-    fruits.swap(0, 1);
-    println!("fruits after swap: {:?}", fruits);
-
-    let first = fruits.first();
-    if let Some(first) = first {
-        println!("First fruit: {}", first);
-    }
-
-    let last = fruits.last();
-    if let Some(last) = last {
-        println!("Last fruit: {}", last);
-    }
-
-    let second = fruits.get(1);
-    if let Some(second) = second {
-        println!("Second fruit: {}", second);
-    }
-
-    let second = fruits[1];
-    println!("Second fruit: {}", second);
-
-    let bunch_of_zeros = vec![0; 5];
-    println!("bunch_of_zeros: {:?}", bunch_of_zeros);
-
-    let mut nums = vec![1, 2, 3, 4];
-    let second_num = nums.remove(1);
-    println!("Removed {} from {:?}", second_num, nums);
-
-    let mut names = vec!["Aaron", "Felicia", "Alex", "Daniel"];
-    names.retain(|name| name.starts_with('A'));
-    println!("Names starting with A: {:?}", names);
-
-    println!("Does 'names' contain \"Alex\"? {}", names.contains(&"Alex"));
-
-
-    let mut nums = vec![1, 2, 2, 3, 4, 4, 4, 5];
-    nums.dedup();
-    println!("Deduped, unsorted nums: {:?}", nums);
-
-    nums.sort();
-    println!("Manually sorted nums: {:?}", nums);
-
-    nums.dedup();
-    println!("Dequped, sorted nums: {:?}", nums);
- 
-    nums.reverse();
-    println!("nums after being reversed: {:?}", nums);
-
-    let mut alphabet = vec!['a', 'b', 'c'];
-    print!("The first two letters of the alphabet are:");
-
-    for letter in alphabet.drain(..2) {
-        print!("{} ", letter);
-    }
     println!();
-    println!("alphabet after being drained: {:?}", alphabet);
 
-    let mut fridge = vec!["Beer", "Leftovers", "Mayonaise"];
-    println!("Is the fridge empty {}", fridge.is_empty());
-    fridge.clear();
-    println!("Is the fridge empty {}", fridge.is_empty());
+    for ch in "ÿ".chars() {
+        println!("{}", ch);
+    }
 
-    let mut colors = vec!["red", "green", "blue", "yellow"];
-    println!("colors before splitting: {:?}", colors);
-    let half = colors.len() / 2;
-    let mut second_half = colors.split_off(half);
-    println!("colors after splitting: {:?}", colors);
-    println!("second_half: {:?}", second_half);
-
-    colors.append(&mut second_half);
-    println!("colors after appending: {:?}", colors);
-    println!("second_half after appending: {:?}", second_half);
-
-    let mut stuff = vec!["1", "2", "3", "4", "5"];
-    println!("Original stuff: {:?}", stuff);
-    let stuff_to_insert = vec!["a", "b", "c"];
-    let removed_stuff: Vec<_> = stuff.splice(1..4, stuff_to_insert).collect();
-    println!("Spliced stuff: {:?}", stuff);
-    println!("Removed stuff: {:?}", removed_stuff);
-
-    // preallocation
-    let mut large_vec: Vec<i32> = Vec::with_capacity(1000000);
-    println!("large_vec after creation:");
-    println!("len:\t\t{}", large_vec.len());
-    println!("capacity:\t{}", large_vec.capacity());
-
-    large_vec.shrink_to_fit();
-    println!("large_vec after shrinking:");
-    println!("len:\t\t{}", large_vec.len());
-    println!("capacity:\t{}", large_vec.capacity());
-
-    let mut nums = vec![1, 2, 3, 4];
-    let second_num = nums.swap_remove(1);
-    println!("Removed {} from {:?}", second_num, nums);
+    println!();
 
 
+    let (first, second) = "HelloThere".split_at(5);
+    println!("first: {}, second: {}", first, second);
+
+    let haiku = "\
+    she watches \n\
+    satisfied after love\n
+    he lies\n\
+    looking up at nothing\n\
+    ";
+    for line in haiku.lines() {
+        println!("\t{}", line);
+    }
+
+    for s in "Never;Give;Up".split(';') {
+        println!("{}", s);
+    }
+
+    let s: Vec<_> = "::Hi::There::".split("::").collect();
+    println!("{:?}", s);
+
+    let s: Vec<_> = "Mr. T.".split_terminator('.').collect();
+    println!("{:?}", s);
+
+    for s in "I'm2fast4you".split(char::is_numeric) {
+        println!("{}", s);
+    }
+
+    for s in "It's not your fault, it's mine".splitn(3, char::is_whitespace) {
+        println!("{}", s);
+    }
+
+    for c in "The Dark Knight rises".matches(char::is_uppercase) {
+        println!("{}", c);
+    }
+
+    let saying = "The early bird gets the worm";
+    let starts_with_the = saying.starts_with("The");
+    println!(
+        "Does \"{}\" start iwth \"The\"?: {}",
+        saying, starts_with_the
+    );
+
+    let starts_with_bird = saying.starts_with("bird");
+    println!(
+        "Does \"{}\" start with \"bird\"?: {}",
+        saying,
+        starts_with_bird
+    );
+
+    let ends_with_worm = saying.ends_with("worm");
+    println!("Does \"{}\" end with \"worm\"?: {}", saying, ends_with_worm);
+
+
+    let contains_bird = saying.contains("bird");
+    println!("Does \"{}\" contain \"bird\"?: {}", saying, contains_bird);
+
+
+    let a_lot_of_whitespace = "  I   love spaaaace   ";
+    let s: Vec<_> = a_lot_of_whitespace.split(' ').collect();
+    println!("{:?}", s);
+    let s: Vec<_> = a_lot_of_whitespace.split_whitespace().collect();
+    println!("{:?}", s);
+
+    let username = "  P3ngu1n\n".trim();
+    println!("{}", username);
+    let username = "   P3ngun1n\n".trim_left();
+    println!("{}", username);
+
+    let username = "   P3ngu1n\n".trim_right();
+    println!("{}", username);
+
+
+    let num = "12".parse::<i32>();
+    if let Ok(num) = num {
+        println!("{} * {} = {}", num, num, num * num);
+    }
+
+    let s = "My dad is the best dad";
+    let new_s = s.replace("dad", "mom");
+    println!("new_s: {}", new_s);
+
+    let lowercase = s.to_lowercase();
+    println!("lowercase: {}", lowercase);
+
+    let uppercase = s.to_uppercase();
+    println!("uppercase: {}", uppercase);
+
+    let greek = "sjdflsd";
+    println!("lowercase greek: {}", greek.to_lowercase());
+
+    let hello = "Hello! ";
+    println!("Three times hello: {}", hello.repeat(3));
 }
