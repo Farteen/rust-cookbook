5d7d4c9b8807ba20d9a781f0634da4364da2f982
farteen <xujavacn@gmail.com>
2020-02-29 15:24:09 +0800

    customized iterator

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,65 @@
-use std::collections::HashSet;
-
 fn main() {
-    let mut books = HashSet::new();
-    books.insert("Harry Potter and the Philosopher's Stone");
-    books.insert("The Name of the Wind");
-    books.insert("A Game of Thrones");
+    let fib: Vec<_> = fibonacci().take(10).collect();
+    println!("First 10 numbers of the fibonacci sequence: {:?}", fib);
 
-    let is_new = books.insert("The Lies of Locke Lamora");
-    if is_new {
-        println!("We've just added a new book!");
+    let mut squared_vec = SquaredVec::new();
+    squared_vec.push(1);
+    squared_vec.push(2);
+    squared_vec.push(3);
+
+    for (index, num) in squared_vec.iter().enumerate() {
+        println!("{}^2 is {}", index + 1, num);
+    }
+}
+
+fn fibonacci() -> Fibonacci {
+    Fibonacci {curr: 0, next: 1}
+}
+
+struct Fibonacci {
+    curr: u32,
+    next: u32
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        let old = self.curr;
+        self.curr = self.next;
+        self.next += old;
+        Some(old)
+    }
+}
+
+use std::ops::Mul;
+struct SquaredVec<T>
+where
+T: Mul + Copy,
+{
+    vec: Vec<T::Output>,
+}
+
+impl<T> SquaredVec<T>
+where
+T: Mul + Copy
+{
+    fn new() -> Self {
+        SquaredVec { vec: Vec::new() }
     }
 
-    let is_new = books.insert("A Game of Thrones");
-    if !is_new {
-        println!("Sorry, we already had that book in store");
+    fn push(&mut self, item: T) {
+        self.vec.push(item * item)
     }
+}
 
-    if !books.contains("The Doors of Stone") {
-        println!("We sadly don't have that book yet");
+use std::ops::Deref;
+impl<T> Deref for SquaredVec<T>
+where
+T: Mul + Copy,
+{
+    type Target = [T::Output];
+    fn deref(&self) -> &Self::Target {
+        &self.vec
     }
-
-    let was_removed = books.remove("The Darkness that comes before");
-    if !was_removed {
-        println!("Couldn't remove book; We didn't have it to begin with");
-    }
-
-    let one_to_five: HashSet<_> = (1..6).collect();
-    let five_to_ten: HashSet<_> = (5..11).collect();
-    let one_to_ten: HashSet<_> = (1..11).collect();
-    let three_to_eight: HashSet<_> = (3..9).collect();
-
-    let is_disjoint = one_to_five.is_disjoint(&five_to_ten);
-    println!("is {:?} disjoint from {:?}?: {}", one_to_ten, five_to_ten, is_disjoint);
-
-    let is_disjoint = one_to_five.is_disjoint(&three_to_eight);
-    println!("is {:?} disjoint from {:?}?: {}", one_to_five, three_to_eight, is_disjoint);
-
-    // difference symetric_difference
-    // intersection union
-
 }
