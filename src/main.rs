e2a1a40607c8329b6d3f1cdf2a8eae41b1cc6b0f
farteen <xujavacn@gmail.com>
2020-02-29 15:54:34 +0800

    using slab connection pool case

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,33 @@
+extern crate slab;
+use slab::{Slab, VacantEntry};
+
 fn main() {
-    let fib: Vec<_> = fibonacci().take(10).collect();
-    println!("First 10 numbers of the fibonacci sequence: {:?}", fib);
+    const CAPACITY: usize = 1024;
+    let mut slab = Slab::with_capacity(CAPACITY);
 
-    let mut squared_vec = SquaredVec::new();
-    squared_vec.push(1);
-    squared_vec.push(2);
-    squared_vec.push(3);
+    let hello_key = slab.insert("hello");
+    let world_key = slab.insert("world");
+    println!("hello_key -> '{}'", slab[hello_key]);
+    println!("world_key -> '{}'", slab[world_key]);
 
-    for (index, num) in squared_vec.iter().enumerate() {
-        println!("{}^2 is {}", index + 1, num);
+    let data_key = {
+        let entry = slab.vacant_entry();
+        fill_some_data(entry)
+    };
+    println!("data_key -> '{}'", slab[data_key]);
+
+    for (key, val) in &slab {
+        println!("{} -> {}", key, val);
+    }
+
+    if slab.len() != slab.capacity() {
+        slab.insert("the slab is not at capacity yet");
     }
 }
 
-fn fibonacci() -> Fibonacci {
-    Fibonacci {curr: 0, next: 1}
-}
-
-struct Fibonacci {
-    curr: u32,
-    next: u32
-}
-
-impl Iterator for Fibonacci {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<u32> {
-        let old = self.curr;
-        self.curr = self.next;
-        self.next += old;
-        Some(old)
-    }
-}
-
-use std::ops::Mul;
-struct SquaredVec<T>
-where
-T: Mul + Copy,
-{
-    vec: Vec<T::Output>,
-}
-
-impl<T> SquaredVec<T>
-where
-T: Mul + Copy
-{
-    fn new() -> Self {
-        SquaredVec { vec: Vec::new() }
-    }
-
-    fn push(&mut self, item: T) {
-        self.vec.push(item * item)
-    }
-}
-
-use std::ops::Deref;
-impl<T> Deref for SquaredVec<T>
-where
-T: Mul + Copy,
-{
-    type Target = [T::Output];
-    fn deref(&self) -> &Self::Target {
-        &self.vec
-    }
+fn fill_some_data(entry: VacantEntry<&str>) -> usize {
+    let data = "Some data";
+    let key = entry.key();
+    entry.insert(data);
+    key
 }
