92918a731a9a2bfc0a8ce303e39fee6df6b85d51
farteen <xujavacn@gmail.com>
2020-02-29 11:01:06 +0800

    using hashset

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,42 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main() {
-    let mut tv_ratings = HashMap::new();
+    let mut books = HashSet::new();
+    books.insert("Harry Potter and the Philosopher's Stone");
+    books.insert("The Name of the Wind");
+    books.insert("A Game of Thrones");
 
-    tv_ratings.insert("The IT Crowd", 8);
-    tv_ratings.insert("13 Reasons Why", 7);
-
-    let contains_tv_show = tv_ratings.contains_key("House of Cards");
-    println!("Did we rate House of Card? {}", contains_tv_show);
-
-    if let Some(rating) = tv_ratings.get("Breaking Bad") {
-        println!("I rate Breaking Bad {} out of 10", rating);
+    let is_new = books.insert("The Lies of Locke Lamora");
+    if is_new {
+        println!("We've just added a new book!");
     }
 
-    let old_rating = tv_ratings.insert("13 Reason Why", 9);
-    if let Some(old_rating) = old_rating {
-        println!("13 Reason Why's old rating was {} out of 10", old_rating);
+    let is_new = books.insert("A Game of Thrones");
+    if !is_new {
+        println!("Sorry, we already had that book in store");
     }
 
-    let removed_value = tv_ratings.remove("The ID Crowd");
-    if let Some(removed_value) = removed_value {
-        println!("The removed series had a rating of {}", removed_value);
+    if !books.contains("The Doors of Stone") {
+        println!("We sadly don't have that book yet");
     }
 
-    println!("All ratings");
-
-    for (key, value) in &tv_ratings {
-        println!("{}\t: {}", key, value);
+    let was_removed = books.remove("The Darkness that comes before");
+    if !was_removed {
+        println!("Couldn't remove book; We didn't have it to begin with");
     }
 
+    let one_to_five: HashSet<_> = (1..6).collect();
+    let five_to_ten: HashSet<_> = (5..11).collect();
+    let one_to_ten: HashSet<_> = (1..11).collect();
+    let three_to_eight: HashSet<_> = (3..9).collect();
 
-    let mut age = HashMap::with_capacity(10);
-    age.insert("Dory", 8);
-    age.insert("Nemo", 5);
-    age.insert("Merlin", 10);
-    age.insert("Bruce", 9);
+    let is_disjoint = one_to_five.is_disjoint(&five_to_ten);
+    println!("is {:?} disjoint from {:?}?: {}", one_to_ten, five_to_ten, is_disjoint);
 
-    println!("All ages:");
-    for age in age.values() {
-        println!("{}", age);
-    }
+    let is_disjoint = one_to_five.is_disjoint(&three_to_eight);
+    println!("is {:?} disjoint from {:?}?: {}", one_to_five, three_to_eight, is_disjoint);
 
-    for age in age.values_mut() {
-        *age *= 10;
-        println!("{}", age);
-    }
+    // difference symetric_difference
+    // intersection union
 
-    {
-        // mut borrow
-        age.entry("coral").or_insert(11);
-    }
-    age.entry("coral").or_insert(15);
-    
 }
