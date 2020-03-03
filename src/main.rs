e669bb30c6d9b01ee1e52f340bd1b6a0b8dfdaaf
farteen <xujavacn@gmail.com>
2020-03-03 22:55:41 +0800

    providing user defined error types

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,62 @@
-trait EmailSender {
-    fn send_email(&self, msg: &Email) -> Option<String>;
+use std::{error, fmt, io, num, result};
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+enum AgeReaderError {
+    Io(io::Error),
+    Parse(num::ParseIntError),
+    NegativeAge(),
 }
 
-#[derive(Debug, Clone)]
-struct Email {
-    from: String,
-    to: String,
-    msg: String,
-}
+type Result<T> = result::Result<T, AgeReaderError>;
 
-#[derive(Debug)]
-struct Customer {
-    address: String,
-    wants_news: bool,
-}
-
-fn publish_news(msg: &str, sender: impl EmailSender, customers: &[Customer]) -> Option<i32> {
-    let mut count = 0;
-    let mut mail = Email {
-        from: "Rust Newsletter".to_string(),
-        to: "".to_string(),
-        msg: msg.to_string(),
-    };
-    for customer in customers {
-        if !customer.wants_news {
-            continue;
-        }
-        mail.to = customer.address.to_string();
-        if sender.send_email(&mail).is_none() {
-            return None;
-        }
-        count += 1;
-    }
-    Some(count)
-}
-
-fn main() {
-
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::cell::RefCell;
-
-    struct MockEmailSender {
-        send_emails: RefCell<Vec<Email>>,
-    }
-    impl MockEmailSender {
-        fn new() -> Self {
-            MockEmailSender {
-                send_emails: RefCell::new(Vec::new()),
-            }
+impl error::Error for AgeReaderError {
+    fn to_string(&self) -> &str {
+        match *self {
+            AgeReaderError::Io(ref err) => err.description(),
+            AgeReaderError::Parse(ref err) => err.description(),
+            AgeReaderError::NegativeAge() => "age is negtive",
         }
     }
 
-    impl EmailSender for MockEmailSender {
-        fn send_email(&self, msg: &Email) -> Option<String> {
-            self.send_emails.borrow_mut().push(msg.clone());
-            Some("200 OK".to_string())
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            AgeReaderError::Io(ref err) => Some(err),
+            AgeReaderError::Parse(ref err) => Some(err),
+            AgeReaderError::NegativeAge() => None,
         }
     }
+}
 
+// impl fmt::Debug for AgeReaderError {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result<(), fmt::Error> {
 
-    #[test]
-    fn sends_zero_to_zero_customers() {
-        let sent = publish_news("hello world!", &MockEmailSender::new(), &[]);
-        assert_eq!(Some(0), sent);
+//     }
+// }
+
+impl fmt::Display for AgeReaderError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            AgeReaderError::Io(ref err) => write!(f, "IO error: {}", err),
+            AgeReaderError::Parse(ref err) => write!(f, "Parse error: {}", err),
+            AgeReaderError::NegativeAge() => write!(f, "Logic error: Age cannot be negtive"),
+        }
     }
 }
+
+impl From<io::Error> for AgeReaderError {
+    fn from(err: io::Error) -> AgeReaderError {
+        AgeReaderError::Io(err)
+    }
+}
+
+impl From<num::ParseIntError> for AgeReaderError {
+    fn from(err: num::ParseIntError) -> AgeReaderError {
+        AgeReaderError::Parse(err)
+    }
+}
+
+
+
+
+fn main() {}
