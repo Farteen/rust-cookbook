60a5610367c9a936bb7b2af990f392c79b7404ea
farteen <xujavacn@gmail.com>
2020-03-03 22:34:56 +0800

    sharing ownership with smart pointers

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,39 @@
-extern crate serde;
-extern crate serde_json;
+use std::rc::Rc;
 
-#[macro_use]
-extern crate serde_derive;
-
-use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
-use std::fs::OpenOptions;
-
-#[derive(Serialize, Deserialize)]
-struct PetOwner {
-    name: String,
-    age: u8,
-    pets: Vec<Pet>,
+struct Kid {
+    ball: Rc<Ball>,
 }
 
-#[derive(Serialize, Deserialize)]
-struct Pet {
-    name: String,
-    species: AllowedSpecies,
-    age: Option<u8>,
-    color: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-enum AllowedSpecies {
-    Dog,
-    Turtle,
-    Cat,
-}
+struct Ball;
 
 fn main() {
-    let file = OpenOptions::new()
-    .read(true)
-    .write(true)
-    .create(true)
-    .open("pet_owner.json")
-    .expect("failed to create json file");
-    
-    let buf_writer = BufWriter::new(&file);
-    write_json(buf_writer).expect("failed to write json");
-    
-    let mut buf_reader = BufReader::new(&file);
-    buf_reader.seek(SeekFrom::Start(0))
-    .expect("failed to jump to the beginning of the JSON file");
-    read_json(buf_reader).expect("failed to read json");
+    {
+        let foo = Rc::new("foo");
+    }
+    {
+        let bar = Rc::new("bar");
+        let second_bar = Rc::clone(&bar);
+    }
+
+    {
+        let baz = Rc::new("baz");
+        {
+            let second_baz = Rc::clone(&baz);
+        }
+    }
+
+    let kid_one = spawn_kid_with_new_ball();
+    let kid_two = Kid {
+        ball: Rc::clone(&kid_one.ball),
+    };
+    let kid_three = Kid{
+        ball: Rc::clone(&kid_one.ball),
+    };
 }
 
-fn write_json<W>(mut writer: W) -> serde_json::Result<()>
-where
-W: Write,
-{
-    let pet_owner = PetOwner {
-        name: "John".to_string(),
-        age: 23,
-        pets: vec![
-            Pet{
-                name: "Waldo".to_string(),
-                species: AllowedSpecies::Dog,
-                age: Some(2),
-                color: None,
-            },
-            Pet {
-                name: "Speedy".to_string(),
-                species: AllowedSpecies::Turtle,
-                age: Some(47),
-                color: Some("Green".to_string()),
-            },
-            Pet {
-                name: "Meows".to_string(),
-                species: AllowedSpecies::Cat,
-                age: None,
-                color: Some("Orange".to_string())
-            },
-        ]
-    };
-    let json = serde_json::to_string(&pet_owner)?;
-    writer.write_all(json.as_bytes())
-    .expect("failed to write file");
-    Ok(())
+fn spawn_kid_with_new_ball() -> Kid {
+    let ball = Rc::new(Ball);
+    Kid {
+        ball: Rc::clone(&ball),
+    }
 }
