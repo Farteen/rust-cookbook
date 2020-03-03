ce60cb0ceba094c35264b5e298c1a3ae560f0d00
farteen <xujavacn@gmail.com>
2020-03-03 22:44:40 +0800

    working with interior mutability

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,71 @@
-use std::rc::Rc;
-
-struct Kid {
-    ball: Rc<Ball>,
+trait EmailSender {
+    fn send_email(&self, msg: &Email) -> Option<String>;
 }
 
-struct Ball;
+#[derive(Debug, Clone)]
+struct Email {
+    from: String,
+    to: String,
+    msg: String,
+}
+
+#[derive(Debug)]
+struct Customer {
+    address: String,
+    wants_news: bool,
+}
+
+fn publish_news(msg: &str, sender: impl EmailSender, customers: &[Customer]) -> Option<i32> {
+    let mut count = 0;
+    let mut mail = Email {
+        from: "Rust Newsletter".to_string(),
+        to: "".to_string(),
+        msg: msg.to_string(),
+    };
+    for customer in customers {
+        if !customer.wants_news {
+            continue;
+        }
+        mail.to = customer.address.to_string();
+        if sender.send_email(&mail).is_none() {
+            return None;
+        }
+        count += 1;
+    }
+    Some(count)
+}
 
 fn main() {
-    {
-        let foo = Rc::new("foo");
-    }
-    {
-        let bar = Rc::new("bar");
-        let second_bar = Rc::clone(&bar);
-    }
 
-    {
-        let baz = Rc::new("baz");
-        {
-            let second_baz = Rc::clone(&baz);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockEmailSender {
+        send_emails: RefCell<Vec<Email>>,
+    }
+    impl MockEmailSender {
+        fn new() -> Self {
+            MockEmailSender {
+                send_emails: RefCell::new(Vec::new()),
+            }
         }
     }
 
-    let kid_one = spawn_kid_with_new_ball();
-    let kid_two = Kid {
-        ball: Rc::clone(&kid_one.ball),
-    };
-    let kid_three = Kid{
-        ball: Rc::clone(&kid_one.ball),
-    };
-}
+    impl EmailSender for MockEmailSender {
+        fn send_email(&self, msg: &Email) -> Option<String> {
+            self.send_emails.borrow_mut().push(msg.clone());
+            Some("200 OK".to_string())
+        }
+    }
 
-fn spawn_kid_with_new_ball() -> Kid {
-    let ball = Rc::new(Ball);
-    Kid {
-        ball: Rc::clone(&ball),
+
+    #[test]
+    fn sends_zero_to_zero_customers() {
+        let sent = publish_news("hello world!", &MockEmailSender::new(), &[]);
+        assert_eq!(Some(0), sent);
     }
 }
