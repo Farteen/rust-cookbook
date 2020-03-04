b80e9ba07cb97fb44c895431d97a426388781735
jiawei <934628017@qq.com>
2020-03-04 11:55:21 +0800

    sending data across threads

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,47 @@
+extern crate rand;
+use rand::Rng;
 use std::thread;
-use std::sync::Arc;
-
+use std::sync::mpsc::channel;
 
 fn main() {
-    let some_resource = Arc::new("Hello world".to_string());
+    let (tx, rx) = channel();
 
-    let thread_a = {
-        let some_resource = some_resource.clone();
+    for i in 0..10 {
+        let tx = tx.clone();
         thread::spawn(move || {
-            println!("Thread A says: {}", some_resource);
-        })
-    };
+            println!("Sending: {}", i);
+            tx.send(i).expect("Disconnect from receiver");
+        });
+    }
 
-    let thread_b = {
-        let some_resource = some_resource.clone();
-        thread::spawn(move || {
-            println!("Thread B says: {}", some_resource);
-        })
-    };
+    for _ in 0..10 {
+        let msg = rx.recv().expect("Disconnected from sender");
+        println!("received: {}", msg);
+    }
 
-    thread_a.join().expect("Thread A panicked");
-    thread_b.join().expect("Thread B panicked");
+    let (tx, rx) = channel();
+    const DISCONNECT: &str = "Goodbye!";
+    thread::spawn(move || {
+        let mut rng = rand::thread_rng();
+
+        loop {
+            let msg = match rng.gen_range(0, 5) {
+                0 => "hi",
+                1 => DISCONNECT,
+                2 => "Howdy there, cowboy",
+                3 => "How are you?",
+                4 => "I'm good, thanks",
+                _ => unreachable!(),
+            };
+            println!("sending: {}", msg);
+            tx.send(msg).expect("Disconnected from receiver");
+            if msg == DISCONNECT {
+                break;
+            }
+        }
+    });
+
+    for msg in rx {
+        println!("received: {}", msg);
+    }
 }
